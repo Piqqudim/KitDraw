243c0ec8189032ef9a457ebdf8ef53c44fcc3d70
Priesthoo <kehindeibirogba42@gmail.com>
2026-08-19 21:51:10 +0100

    Added the Shader Object interface that is directly communicating with opengl instead of declaring the function explicitly in my program

--- src/Renderer/Shader.cpp
+++ src/Renderer/Shader.cpp
@@ -4,7 +4,7 @@
 #include <stdexcept>
 #include <vector>
 #include <utility>
-
+#include<fstream>
 using namespace gl33core;
 using namespace std;
 
@@ -21,7 +21,7 @@ namespace KitDraw::render {
                 GLint logLength = 0;
                 glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &logLength);
                 vector<char> log(static_cast<size_t>(logLength));
-                glGetShaderInfoLog(shader,logLength, nullpty, log.data());
+                glGetShaderInfoLog(shader,logLength, nullptr, log.data());
                 glDeleteShader(shader);
                 cout<<"Shader compile error:"<<string(log.begin(), log.end()) << endl;
             }
@@ -29,5 +29,61 @@ namespace KitDraw::render {
         }
     }// namespace
 
-    Shader::Shader()
+    Shader::Shader(const string& vertexSource,const std::string& fragmentSource){
+        GLuint vs = compile(GL_VERTEX_SHADER,vertexSource);
+        GLuint fg = compile(GL_FRAGMENT_SHADER,fragmentSource);
+
+        program = glCreateProgram();
+        glAttachShader(program_, vs);
+        glAttachShader(program_, fs);
+        glLinkProgram(program_);
+
+        GLint success = 0;
+        glGetProgramiv(program_,GL_LINK_STATUS, &success);
+        if(!success){
+            GLint logLength = 0;
+            glGetProgramiv(program_, GL_INFO_LOG_LENGTH, &logLength);
+            std::vector<char> log(static_cast<size_t>(logLength));
+            glGetProgramInfoLog(program_, logLength, nullptr, log.data());
+            glDeleteShader(vs);
+            glDeleteShader(fs);
+            glDeleteProgram(program_);
+            cout<<"Shader link error:"+ string(log.begin(), log.end())<<endl;
+        }
+        glDeleteShader(vs);
+        glDeleteShader(fs);
+        
+    }
+    Shader::~Shader() { if (program_) glDeleteProgram(program_); }
+
+    Shader::Shader(Shader&& other) noexcept: program_(exchange(other.program_, 0)) {}
+
+    Shader& Shader::operator=(Shader&& other) noexcept {
+        if(this != &other){
+            if(program_) glDeleteProgram(program_);
+            program_ = exchange(other.program_, 0);
+        }
+        return *this
+    }
+
+    void Shader::bind() const { glUseProgram(program_); }
+
+    void Shader::setUniformMat4(const char* name, const glm::mat4& value) const {
+        glUniformMatrix4fv(glGetUniformLocation(program_, name), 1, GL_FALSE, glm::value_ptr(value));
+    }
+    void Shader::setUniformInt(const char* name, int value) const{
+        glUniform1i(glGetUniformLocation(program_, name), value);
+    }
+    void Shader::setUniformFloat(const char* name, float value) const {
+        glUniform1f(glGetUniformLocation(program_, name), value);
+    }
+    void Shader::setUniformVec2(const char* name, const glm::vec2& value) const {
+        glUniform2fv(glGetUniformLocation(program_, name), 1, glm::value_ptr(value));
+    }
+    void Shader::setUniformVec3(const char* name, const glm::vec3& value) const {
+        glUniform3fv(glGetUniformLocation(program_,name), glm::value_ptr(value));
+    }
+    void Shader::setUniformVec4(const char* name, const glm::vec4& value) const {
+        glUniform4fv(glGetUniformLocation(program_, name), glm::value_ptr(value));
+    }
 }
